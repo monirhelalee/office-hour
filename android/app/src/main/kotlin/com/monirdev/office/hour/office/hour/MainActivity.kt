093a4ff379bc08d83a06f1b0f073ad28b6745104
monir <mhelalee@wingsfin.com>
2025-06-24 10:00:26 +0600

    Bisbillah

---
+++ android/app/src/main/kotlin/com/monirdev/office/hour/office/hour/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.monirdev.office_hour.office_hour
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:office_hour/app/app.dart';
+import 'package:office_hour/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
